4205e2afa00c1b2b84f300d778bfaad2e46884f9
M-asael <130579509+M-asael@users.noreply.github.com>
2024-11-08 12:03:08 -0300

    Update arduino_main.ino

--- src/arduino_main/arduino_main.ino
+++ src/arduino_main/arduino_main.ino
@@ -1,71 +1,132 @@
-/* @autor: Eletrogate
-   @licença: GNU GENERAL PUBLIC LICENSE Version 3 */
+#define in1 3
+#define in2 5
+#define in3 9
+#define in4 10
 
-#include "constantes.h" // inclui cabeçalho com as constantes
+#define byte_inicial ':'
+#define byte_final '\n'
 
-bool dado_novo;                                       // declara as
-uint8_t v, a, r, vel_int, val_mA, val_mB, quadrante;  // variaveis que serao
-char vel[4], angulo[4], recebido[16], c;              // utilizada ao longo
-unsigned  ang_int;                                    // do programa
+#define estado_inicial 0
+#define estado_botao 1
+#define estado_velocidade 2
+#define estado_angulo 3
+#define estado_final 4
 
-bool caractereValido(char c)  {
-  return ((c >= '0' && c <= '9') || c == ' ');  // verifica se o caractere recebido do ESP é um número ou um espaço
-}
+#define bytes_pacote (1 + 3 + 3)
+
+uint8_t estado;
+uint8_t recebidos, recebidosItem;
+uint8_t botao;
+char velocidade[4];
+char angulo[4];
 
 void setup() {
-  delay(500);                 // aguarda o sistema estabilizar
-  Serial.begin(9600);         // inicia a serial
-  pinMode(pinIn, INPUT);      // configura e inicia
-  pinMode(pinOut, OUTPUT);    // as entradas
-  digitalWrite(pinOut, LOW);  // e saídas
+  estado = estado_inicial;
+  recebidos = 0;
+  botao = 0;
+  delay(500); // aguarda o sistema estabilizar
+  Serial.begin(9600); // inicia a serial
 }
 
+
 void loop() {
+  if (!Serial.available())
+    return;
 
-  if(!digitalRead(pinIn)) {           // se detectar nova conexão à página
-    digitalWrite(pinOut, LOW);        // avisa que detectou
-    delay(100);                       // aguarda 100 milisegundos
-    while(Serial.available())
-      Serial.read();                  // esvazia o buffer
-    digitalWrite(pinOut, HIGH);       // prepara para a proxima conexão
+  const uint8_t recebido = Serial.read();
+  switch (recebido) {
+    case byte_inicial:
+      estado = estado_botao;
+      recebidos = 0;
+      return;
+    case byte_final:
+      if (recebidos != bytes_pacote) {
+        estado = estado_inicial;
+        recebidos = 0;
+        return;
+      }
+      estado = estado_inicial;
+      recebidos = 0;
+      break;
+    default:
+      if (recebido < '0' || recebido > '9' || recebidos >= bytes_pacote) {
+        estado = estado_inicial;
+        recebidos = 0;
+        return;
+      }
+      recebidos++;
+      switch (estado) {
+        case estado_botao:
+          botao = (recebido != '0');
+          recebidosItem = 0;
+          estado = estado_velocidade;
+          return;
+        case estado_velocidade:
+          velocidade[recebidosItem] = recebido;
+          recebidosItem++;
+          if (recebidosItem >= 3) {
+            recebidosItem = 0;
+            velocidade[3] = 0;
+            estado = estado_angulo;
+          }
+          return;
+        case estado_angulo:
+          angulo[recebidosItem] = recebido;
+          recebidosItem++;
+          if (recebidosItem >= 3) {
+            angulo[3] = 0;
+            estado = estado_final;
+          }
+          return;
+        default:
+          estado = estado_inicial;
+          recebidos = 0;
+          return;
+      }
+      return;
   }
 
-  if(Serial.available() && Serial.read() == caractereInicio) {  // se há dados e o primeiro é caractereInicio
-  dado_novo = true;                                   // armazena que há dado novo
-    r = -1;                                           // prepara o indice de recebido
-    while((c = Serial.read()) != caractereFinal)      // enquanto nao for o caractere finalizador
-      if(caractereValido(c))                          // se for um caractere valido
-        recebido[++ r] = c;                           // o armazena a incrementa o indice
+  // transforma a velocidade em um inteiro e, então, o mapeia entre 0 e 255
+  int vel_int = atoi(velocidade);
+  vel_int = map(vel_int, 0, 100, 0, 255);
 
-    for(v = 0; recebido[v] != caractereSepara; v ++)  // do primeiro caractere até caractereSepara
-      vel[v] = recebido[v];                           // copia-o para vel
-    vel[v] = '\0';                                    // insere o caractere nulo na posição posterior à do ultimo copiado
+  // transforma o angulo em um inteiro
+  int ang_int = atoi(angulo);
 
-    for(a = v + 1; a <= r; a ++)                      // do primeiro após caractereSepara até o ultimo caractere valido de recebido
-      angulo[a - v - 1] = recebido[a];                // copia-o para angulo - v - 1 é a diferença entre a posição de recebido e a de angulo
+  /*
+  Serial.println("----------");
+  Serial.print("b: ");
+  Serial.println(botao ? "1" : "0");
+  Serial.print("v: ");
+  Serial.println(vel_int);
+  Serial.print("a: ");
+  Serial.println(ang_int);
+  */
 
-    angulo[r - v - 1 < 3 ?      // angulo recebeu até 3 caracteres?
-            r - v : 3] = '\0';  // se sim, insere o caractere nulo na posição posterior à do ultimo copiado. se não, o insere na ultima posição de angulo
+  uint8_t quadrante;
+  if (ang_int < 90)
+    quadrante = 1;
+  else if (ang_int < 180)
+    quadrante = 2;
+  else if (ang_int < 270)
+    quadrante = 3;
+  else
+    quadrante = 4;
+
+  uint8_t val_mA, val_mB;
+  if (quadrante == 1 || quadrante == 4) {                                     // se estiver na direita
+    val_mB = (uint8_t) vel_int;                                               // o motor B recebe a velocidade indicada no joystick
+    val_mA = (uint8_t) (vel_int * (1.0 - cos(ang_int * PI / 180.0) / 2.0));   // o motor A recebe esta velocidade reduzida proporcionalmente ao cosseno do ângulo
+  } else {                                                                    // caso não
+    val_mB = (uint8_t) (vel_int * (1.0 + cos(ang_int * PI / 180.0) / 2.0));   // o motor B recebe a velocidade acrescida proporcionalmente ao cosseno (que será negativo) do ângulo
+    val_mA = (uint8_t) vel_int;                                               // o motor A recebe a velocidade indicada no joystick
   }
-
-  if(dado_novo) { // se recebeu um novo dado
-    vel_int = atoi(vel);  vel_int = map(vel_int, 0, 100, 0, 255); // transforma a velocidade em um inteiro e, então, o mapeia entre 0 e 255
-    ang_int = atoi(angulo);                                       // transforma o angulo em um inteiro
-
-    if(ang_int < 90) quadrante = 1; else if(ang_int < 180) quadrante = 2; else if(ang_int < 270) quadrante = 3; else if(ang_int < 360) quadrante = 4; // determina em qual quadrante o joystick está
-    
-    if(quadrante == 1 || quadrante == 4) {                                      // se estiver na direita
-      val_mB = (uint8_t) vel_int;                                               // o motor B recebe a velocidade indicada no joystick
-      val_mA = (uint8_t) (vel_int * (1.0 - cos(ang_int * PI / 180.0) / 2.0)); } // o motor A recebe esta velocidade reduzida proporcionalmente ao cosseno do ângulo
-    else {                                                                      // caso não
-      val_mB = (uint8_t) (vel_int * (1.0 + cos(ang_int * PI / 180.0) / 2.0));   // o motor B recebe a velocidade acrescida proporcionalmente ao cosseno (que será negativo) do ângulo
-      val_mA = (uint8_t) vel_int; }                                             // o motor A recebe a velocidade indicada no joystick
-
-    if(quadrante < 3) {                                 // se estiver em cima/na frente
-      analogWrite(in2, val_mA); analogWrite(in1, 0);    // aciona os motores
-      analogWrite(in4, val_mB); analogWrite(in3, 0);  } // para frente
-    else {                                              // senão
-      analogWrite(in2, 0); analogWrite(in1, val_mA);    // aciona os motores
-      analogWrite(in4, 0); analogWrite(in3, val_mB);  } // para trás
-  } dado_novo = false;                                  // indica que o dado já foi tratado
+  
+  if (quadrante < 3) {                                // se estiver em cima/na frente
+    analogWrite(in2, val_mA); analogWrite(in1, 0);    // aciona os motores
+    analogWrite(in4, val_mB); analogWrite(in3, 0);    // para frente
+  } else {                                            // senão
+    analogWrite(in2, 0); analogWrite(in1, val_mA);    // aciona os motores
+    analogWrite(in4, 0); analogWrite(in3, val_mB);    // para trás
+  }
 }
