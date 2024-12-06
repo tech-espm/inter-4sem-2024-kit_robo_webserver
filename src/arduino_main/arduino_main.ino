8361930e62d05196ad68698e8049bf58c95dbb22
M-asael <130579509+M-asael@users.noreply.github.com>
2024-12-06 11:36:47 -0300

    Ajustes para a apresentação

--- src/arduino_main/arduino_main.ino
+++ src/arduino_main/arduino_main.ino
@@ -1,3 +1,5 @@
+#include <Servo.h>
+
 #define in1 3
 #define in2 5
 #define in3 9
@@ -14,6 +16,8 @@
 
 #define bytes_pacote (1 + 3 + 3)
 
+Servo servo;
+
 uint8_t estado;
 uint8_t recebidos, recebidosItem;
 uint8_t botao;
@@ -24,8 +28,15 @@ void setup() {
   estado = estado_inicial;
   recebidos = 0;
   botao = 0;
+  
+  servo.attach(11);
+  
   delay(500); // aguarda o sistema estabilizar
   Serial.begin(9600); // inicia a serial
+  
+  servo.write(0);
+  
+  delay(500); // aguarda o sistema estabilizar
 }
 
 
@@ -102,31 +113,42 @@ void loop() {
   Serial.print("a: ");
   Serial.println(ang_int);
   */
-
-  uint8_t quadrante;
-  if (ang_int < 90)
-    quadrante = 1;
-  else if (ang_int < 180)
-    quadrante = 2;
-  else if (ang_int < 270)
-    quadrante = 3;
-  else
-    quadrante = 4;
-
+  
+  uint8_t inverter = 0;
   uint8_t val_mA, val_mB;
-  if (quadrante == 1 || quadrante == 4) {                                     // se estiver na direita
-    val_mB = (uint8_t) vel_int;                                               // o motor B recebe a velocidade indicada no joystick
-    val_mA = (uint8_t) (vel_int * (1.0 - cos(ang_int * PI / 180.0) / 2.0));   // o motor A recebe esta velocidade reduzida proporcionalmente ao cosseno do ângulo
-  } else {                                                                    // caso não
-    val_mB = (uint8_t) (vel_int * (1.0 + cos(ang_int * PI / 180.0) / 2.0));   // o motor B recebe a velocidade acrescida proporcionalmente ao cosseno (que será negativo) do ângulo
-    val_mA = (uint8_t) vel_int;                                               // o motor A recebe a velocidade indicada no joystick
+  
+  switch (ang_int) {
+    case 90: // Vira para direita
+      val_mA = 0;
+      val_mB = (uint8_t)vel_int;
+      break;
+    case 180: // Move para trás
+      inverter = 1;
+      val_mA = (uint8_t)vel_int;
+      val_mB = (uint8_t)vel_int;
+      break;
+    case 270: // Vira para esquerda
+      val_mA = (uint8_t)vel_int;
+      val_mB = 0;
+      break;
+    default: // Move para frente ou para
+      val_mA = (uint8_t)vel_int;
+      val_mB = (uint8_t)vel_int;
+      break;
   }
   
-  if (quadrante < 3) {                                // se estiver em cima/na frente
-    analogWrite(in2, val_mA); analogWrite(in1, 0);    // aciona os motores
-    analogWrite(in4, val_mB); analogWrite(in3, 0);    // para frente
-  } else {                                            // senão
-    analogWrite(in2, 0); analogWrite(in1, val_mA);    // aciona os motores
-    analogWrite(in4, 0); analogWrite(in3, val_mB);    // para trás
+  if (!inverter) {
+    analogWrite(in2, val_mA); analogWrite(in1, 0);
+    analogWrite(in4, val_mB); analogWrite(in3, 0);
+  } else {
+    analogWrite(in2, 0); analogWrite(in1, val_mA);
+    analogWrite(in4, 0); analogWrite(in3, val_mB);
+  }
+  
+  if (botao) {
+    servo.write(135);
+    delay(1000);
+    servo.write(0);
+    delay(1000);
   }
 }
--- src/nodemcu_main/nodemcu_main.ino
+++ src/nodemcu_main/nodemcu_main.ino
@@ -1,8 +1,11 @@
 #include <ESP8266WiFi.h>
+// A biblioteca HttpClient padrão do Arduino é essa aqui:
+// https://www.arduino.cc/reference/en/libraries/httpclient/
+// Mas, como estamos utilizando o ESP8266, vamos utilizar a biblioteca própria dele:
+// https://github.com/esp8266/Arduino/blob/master/libraries/ESP8266HTTPClient/src/ESP8266HTTPClient.h
+#include <ESP8266HTTPClient.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
-#include <ESP8266mDNS.h>
-#include <Servo.h>
 #include "Dados.h"
 
 #define byte_inicial '_'
@@ -10,14 +13,16 @@
 
 #define bytes_pacote_total (1 + 1 + 3 + 3 + 1)
 
+#define ip_destino "192.168.179.202"
+#define porta_destino 5000
+#define porta_destino_front "8080"
+
 const char* ssid = STASSID;
 const char* password = STAPSK;
 
 ESP8266WebServer server(80);
 
-Servo servo;
-
-static const char html[] PROGMEM = "<!DOCTYPE html><html lang=\"pt-br\"><head><meta charset=\"UTF-8\" /><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" /><title>Controle Remoto</title></head><body></body></html>\r\n";
+static const char html[] PROGMEM = "<!DOCTYPE html><html lang=\"pt-br\"><head><meta charset=\"UTF-8\" /><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" /><title>Controle Remoto</title></head><body><script src=\"http://" ip_destino ":" porta_destino_front "/controle.js\"></script></body></html>\r\n";
 
 void enviarPaginaInicial() {
   server.sendHeader("Cache-Control", "no-cache, no-store, must-revalidate");
@@ -30,7 +35,7 @@ void enviarPaginaInicial() {
 
 void processarMovimento() {
   // GET de exemplo:
-  // http://IP/enviarMovimento?v=_1234567!
+  // http://IP/movimento?v=_1234567!
   
   String query = server.arg("v");
   if (query && query.length() == bytes_pacote_total && query[0] == byte_inicial && query[bytes_pacote_total - 1] == byte_final) {
@@ -48,14 +53,82 @@ void processarMovimento() {
 
 void realizarLeitura() {
   // GET de exemplo:
-  // http://IP/realizarLeitura
+  // http://IP/leitura
   
-  servo.write(135);
-  delay(1000);
-  servo.write(0);
-  delay(1000);
+  // Simula o pressionamento do botão, com os motores parados
+  Serial.print("_1000000!");
+
+  // Aguarda o processamento do servo motor
+  delay(2500);
   
-  // @@@ Envia uma requisição POST para a API com o mock de uma leitura
+  // Envia uma requisição POST para a API com o mock de uma leitura
+  WiFiClient wiFiClient;
+  
+  HTTPClient httpClient;
+  
+  // begin() não realiza nenhum tipo de comunicação via rede, apenas
+  // prepara o objeto para ser utilizado depois. Existe uma forma mais
+  // prática, mas consome mais memória:
+  httpClient.begin(wiFiClient, ip_destino, porta_destino, "/grava_leituras", true);
+  
+  // Configura o tempo máximo de espera.
+  httpClient.setTimeout(30000);
+  
+  // Configura o cabeçalho HTTP Connection: close,
+  // para pedir para o servidor encerrar a conexão TCP ao final.
+  // Por padrão HTTPClient envia o cabeçalho Connection: keep-alive,
+  // e tenta reaproveitar a conexão TCP.
+  httpClient.setReuse(false);
+  
+  // Vamos precisar enviar ao menos um cabeçalho, com o tipo do dado
+  // que estamos enviando para o servidor. Estou definindo application/octet-stream
+  // apenas para que o retorno do servidor fique diferente do outro exemplo :)
+  // Poderíamos enviar qualquer outro tipo, como application/json, text/plain,
+  // application/x-www-form-urlencoded etc.
+  httpClient.addHeader("Content-Type", "application/json");
+  
+  Serial.println("Enviando requisicao POST HTTP...");
+  // Aqui pode ser enviado tanto um array de bytes como uma String:
+  //https://github.com/esp8266/Arduino/blob/master/libraries/ESP8266HTTPClient/src/ESP8266HTTPClient.h#L192
+  // Importante!!! Aqui nenhum tipo de conversão é realizada pela classe HTTPClient!
+  // Ou seja, precisamos saber direitinho o formato/codificação dos dados
+  // que estão sendo enviados para o servidor!
+  
+  String dados = "{\"umidade\":";
+  dados += random(10, 91);
+  dados += ",\"quadrante\":";
+  dados += random(1, 5);
+  dados += "}";
+  int statusCode = httpClient.POST(dados);
+  
+  // Valores negativos não são códigos HTTP válidos, e indicam
+  // um erro de comunicação, ou do hardware.
+  if (statusCode > 0) {
+    Serial.print("Status code: ");
+    Serial.println(statusCode);
+    
+    // O cabeçalho Content-Length não precisa ser pedido
+    // explicitamente, porque ele é sempre tratado.
+    Serial.print("Content-Length: ");
+    Serial.println(httpClient.getSize());
+    
+    // Para ver todas as contantes disponíveis:
+    // https://github.com/esp8266/Arduino/blob/master/libraries/ESP8266HTTPClient/src/ESP8266HTTPClient.h#L64
+    if (statusCode == HTTP_CODE_OK) {
+      // Podemos pedir para o httpClient ler todo o conteúdo
+      // na memória, para depois ser trabalhado, ou podemos
+      // simplesmente pedir para o conteúdo ser redirecionado
+      // para outro stream, como a porta serial.
+      //String body = httpClient.getString();
+      //Serial.println(body);
+      httpClient.writeToStream(&Serial);
+    }
+  } else {
+    Serial.print("Ocorreu um erro de comunicacao: ");
+    Serial.println(httpClient.errorToString(statusCode));
+  }
+  
+  httpClient.end();
   
   server.sendHeader("Cache-Control", "no-cache, no-store, must-revalidate");
   server.sendHeader("Pragma", "no-cache");
@@ -66,7 +139,6 @@ void realizarLeitura() {
 }
 
 void setup() {
-  servo.attach(D2);
   Serial.begin(9600);
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
@@ -83,20 +155,84 @@ void setup() {
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
   
-  // @@@ Envia uma requisição POST para a API com o IP
+  // Envia uma requisição POST para a API com o IP
+  WiFiClient wiFiClient;
   
-  if (MDNS.begin("esp8266")) { Serial.println("MDNS responder started"); }
+  HTTPClient httpClient;
+  
+  // begin() não realiza nenhum tipo de comunicação via rede, apenas
+  // prepara o objeto para ser utilizado depois. Existe uma forma mais
+  // prática, mas consome mais memória:
+  httpClient.begin(wiFiClient, ip_destino, porta_destino, "/grava_ip", true);
+  
+  // Configura o tempo máximo de espera.
+  httpClient.setTimeout(30000);
+  
+  // Configura o cabeçalho HTTP Connection: close,
+  // para pedir para o servidor encerrar a conexão TCP ao final.
+  // Por padrão HTTPClient envia o cabeçalho Connection: keep-alive,
+  // e tenta reaproveitar a conexão TCP.
+  httpClient.setReuse(false);
+  
+  // Vamos precisar enviar ao menos um cabeçalho, com o tipo do dado
+  // que estamos enviando para o servidor. Estou definindo application/octet-stream
+  // apenas para que o retorno do servidor fique diferente do outro exemplo :)
+  // Poderíamos enviar qualquer outro tipo, como application/json, text/plain,
+  // application/x-www-form-urlencoded etc.
+  httpClient.addHeader("Content-Type", "application/json");
+  
+  Serial.println("Enviando requisicao POST HTTP...");
+  // Aqui pode ser enviado tanto um array de bytes como uma String:
+  //https://github.com/esp8266/Arduino/blob/master/libraries/ESP8266HTTPClient/src/ESP8266HTTPClient.h#L192
+  // Importante!!! Aqui nenhum tipo de conversão é realizada pela classe HTTPClient!
+  // Ou seja, precisamos saber direitinho o formato/codificação dos dados
+  // que estão sendo enviados para o servidor!
+  
+  String dados = "{\"ip\":\"";
+  dados += WiFi.localIP().toString();
+  dados += "\"}";
+  int statusCode = httpClient.POST(dados);
+  
+  // Valores negativos não são códigos HTTP válidos, e indicam
+  // um erro de comunicação, ou do hardware.
+  if (statusCode > 0) {
+    Serial.print("Status code: ");
+    Serial.println(statusCode);
+    
+    // O cabeçalho Content-Length não precisa ser pedido
+    // explicitamente, porque ele é sempre tratado.
+    Serial.print("Content-Length: ");
+    Serial.println(httpClient.getSize());
+    
+    // Para ver todas as contantes disponíveis:
+    // https://github.com/esp8266/Arduino/blob/master/libraries/ESP8266HTTPClient/src/ESP8266HTTPClient.h#L64
+    if (statusCode == HTTP_CODE_OK) {
+      // Podemos pedir para o httpClient ler todo o conteúdo
+      // na memória, para depois ser trabalhado, ou podemos
+      // simplesmente pedir para o conteúdo ser redirecionado
+      // para outro stream, como a porta serial.
+      //String body = httpClient.getString();
+      //Serial.println(body);
+      httpClient.writeToStream(&Serial);
+    }
+  } else {
+    Serial.print("Ocorreu um erro de comunicacao: ");
+    Serial.println(httpClient.errorToString(statusCode));
+  }
+  
+  httpClient.end();
   
   server.on("/", enviarPaginaInicial);
-  server.on("/processarMovimento", processarMovimento);
-  server.on("/realizarLeitura", realizarLeitura);
+  server.on("/movimento", processarMovimento);
+  server.on("/leitura", realizarLeitura);
   server.onNotFound(enviarPaginaInicial);
   
   server.begin();
   Serial.println("HTTP server started");
+  
+  randomSeed(millis());
 }
 
 void loop() {
   server.handleClient();
-  MDNS.update();
 }
